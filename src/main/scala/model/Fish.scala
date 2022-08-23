35d912c2bd18a882a18816c3938ce00731eed4ec
Ledmington <68538713+Ledmington@users.noreply.github.com>
2022-08-24 00:11:40 +0200

    un sacco di roba

--- src/main/scala/model/Fish.scala
+++ src/main/scala/model/Fish.scala
@@ -5,8 +5,5 @@ import model.Fish.MAX_HUNGER
 object Fish:
   val MAX_HUNGER: Int = 100
 
-class Fish:
-  val age: Int = 0
-  val hunger: Int = MAX_HUNGER
-
+case class Fish(val age: Int = 0, val hunger: Int = MAX_HUNGER):
   def isDead: Boolean = hunger == 0
